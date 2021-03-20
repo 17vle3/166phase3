14ee0163f72f09c4f4e9bf87695d605f59e5d644
17vle3 <17vle3@gmail.com>
2021-03-19 23:10:25 -0700

    Update DBproject.java

--- java/src/DBproject.java
+++ java/src/DBproject.java
@@ -869,7 +869,7 @@ public class DBproject{
 		//order by repairs DESC
 		
 		try {
-			String query ="SELECT COUNT(*) \n FROM Repairs \n  GROUP BY Ship \n ORDER BY Repairs DESC;";			
+			String query ="SELECT COUNT(*) \n FROM Repairs \n  GROUP BY ship_id \n ORDER BY Repairs DESC;";			
 			esql.executeQueryAndPrintResult(query);
 		}
 		catch(Exception e) {
